ea7667263ea9a443865b13e01ac0c5b25df813e5
Ian McNichols <90727326+ian-mcnichols@users.noreply.github.com>
2021-11-01 13:23:02 -0500

    Update 480_HW1_c.py

--- 480_HW1_c.py
+++ 480_HW1_c.py
@@ -1,17 +1,48 @@
 import csv
 import pandas
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
 
-def write_data_file(display=False):
-    files = ['data/sensoringData_magn_changed.csv', 'data/sensoringData_gyro_changed.csv',
-             'data/sensoringData_acc_changed.csv', 'data/location_data1_changed.csv']
-    magn_df = pandas.read_csv(files[0])
-    gyro_df = pandas.read_csv(files[1])
-    acc_df = pandas.read_csv(files[2])
-    loc_df = pandas.read_csv(files[3])
-    print("csv's loaded")
-    magn_gyro_merge = pandas.concat([magn_df, gyro_df, acc_df, loc_df])
-    print(magn_gyro_merge)
+def convert_to_int(activity):
+    if activity == "Walking":
+        return 1
+    elif activity == "Active":
+        return 2
+    elif activity == "Inactive":
+        return 3
+    elif activity == "Driving":
+        return 4
 
 
-write_data_file(display=False)
+files = ['data/sensoringData_magn_changed.csv', 'data/sensoringData_gyro_changed.csv',
+            'data/sensoringData_acc_changed.csv', 'data/location_data1_changed.csv']
+#magn_df = pandas.read_csv(files[0])
+#gyro_df = pandas.read_csv(files[1])
+#acc_df = pandas.read_csv(files[2])
+#loc_df = pandas.read_csv(files[3])
+# Load data
+merge = pandas.read_csv('data/merged_data.csv')
+print("csv's loaded")
+# Clean up data
+merge = merge.drop(['Unnamed: 0.1'], axis=1)
+merge = merge.fillna(-1)
+merge['activity'] = np.where(merge['activity'] == "Walking", 1, merge['activity'])
+merge['activity'] = np.where(merge['activity'] == " Walking", 1, merge['activity'])
+merge['activity'] = np.where(merge['activity'] == "Active", 2, merge['activity'])
+merge['activity'] = np.where(merge['activity'] == " Active", 2, merge['activity'])
+merge['activity'] = np.where(merge['activity'] == "Inactive", 3, merge['activity'])
+merge['activity'] = np.where(merge['activity'] == " Inactive", 3, merge['activity'])
+merge['activity'] = np.where(merge['activity'] == " Driving", 4, merge['activity'])
+merge['activity'] = np.where(merge['activity'] == "Driving", 4, merge['activity'])
+# Display data
+print(merge)
+pause = input("Pausing...")
+# Run Linear Regression
+regression_model = LinearRegression()
+y = merge.activity
+regression_model.fit(merge, y)
+# Display output
+print("Linear regression score:", regression_model.score(merge, y))
+
+#merge.to_csv('data/merged_data.csv')
